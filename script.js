96a82146103cfc6a71004a86d96dbe48b0c825cd
Luis Willer <alemaowiller@gmail.com>
2023-04-24 20:04:49 -0300

    updated background construction inside page

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function toggleMode() {
+  // pegar o elemento raiz HTML
   const html = document.documentElement
+  // alternar a classe "light"
   html.classList.toggle("light")
 
   // pegar a tag img
   const img = document.querySelector("#profile img")
 
-  // e substituir a imagem
+  // e substituir a imagem dependendo do modo escolhido
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/logoSoftLight.png")
   } else {
     img.setAttribute("src", "./assets/logoSoft.png")
   }
-  
 }
