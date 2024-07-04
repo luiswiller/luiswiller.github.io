422c50630b4a3ecb8782d3997fc3510072ee861e
luiswiller <alemaowiller@gmail.com>
2024-07-04 19:26:18 -0300

    scriptAddress updated

--- form/scriptAddress.js
+++ form/scriptAddress.js
@@ -1,3 +1,10 @@
+document.getElementById("cep").addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault() // Impede o comportamento padrão de submit
+    buscarCEP()
+  }
+})
+
 function buscarCEP() {
   const cep = document.getElementById("cep").value
   if (cep.length !== 8) {
@@ -16,6 +23,9 @@ function buscarCEP() {
       document.getElementById("bairro").value = data.bairro
       document.getElementById("city").value = data.localidade
       document.getElementById("state").value = data.uf
+
+      // Após preencher os campos, move o foco para o campo (Número)
+      document.getElementById("number").focus()
     })
     .catch((error) => {
       console.error("Erro ao buscar CEP:", error)
