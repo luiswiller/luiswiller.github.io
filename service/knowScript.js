77c7f1ef53fb9ab589391fdc3e69f766bea9eafb
luiswiller <alemaowiller@gmail.com>
2024-07-05 10:14:15 -0300

    Remove unnecessary JavaScript file

--- service/knowScript.js
+++
@@ -1,8 +0,0 @@
-function chamar() {
-  document.querySelector("#enterInside").style.opacity = 1
-  document.querySelector("#colorBody").style.opacity = 0.2
-}
-function toClose() {
-  document.querySelector("#colorBody").style.opacity = 1
-  document.querySelector("#enterInside").style.opacity = 0
-}
