fab0e651e86abcfe019ea39c00fe19fed77dd64b
luiswiller <alemaowiller@gmail.com>
2024-07-05 09:35:20 -0300

    js organized

---
+++ js/knowScript.js
@@ -0,0 +1,8 @@
+function chamar() {
+  document.querySelector("#enterInside").style.opacity = 1
+  document.querySelector("#colorBody").style.opacity = 0.2
+}
+function toClose() {
+  document.querySelector("#colorBody").style.opacity = 1
+  document.querySelector("#enterInside").style.opacity = 0
+}
